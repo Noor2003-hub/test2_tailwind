036244d8de9d466438bbc915f202cc5137cc3fa9
Noor2003-hub <sun.n00r.n@gmail.com>
2025-04-01 00:31:40 +0300

    tailwind on whyJoin section

--- my-training-website/src/app/components/HeroSection.tsx
+++ my-training-website/src/app/components/HeroSection.tsx
@@ -31,171 +31,17 @@ const HeroSection = () => {
   ];
   const redCircleSizes = [25, 18, 10, 18];
   return (
-    // <div className="flex w-full justify-between bg-[#EAF2FF]">
-    //   <div className="flex flex-col gap-4.5 !pt-25 !pb-50 !pl-40">
-    //     <h6 className="text-[#23A6F0]">Online training</h6>
-    //     <h1 className="w-2 text-5xl leading-18 font-bold text-[#252B42]">
-    //       Highly Distinguished Courses
-    //     </h1>
-    //     <p className="w-90 text-xl font-light text-[#737373]">
-    //       We know how large objects will act, but things on a small scale
-    //     </p>
-    //   </div>
-    //   <div className="w-200 !pl-30 text-white">
-    //     <div
-    //       style={{
-    //         position: "relative",
-    //         marginRight: 70,
-    //         width: "500px",
-    //         height: "500px",
-    //       }}
-    //     >
-    //       <svg
-    //         width="100%"
-    //         height="100%"
-    //         style={{ position: "absolute", top: 0, left: 0 }}
-    //       >
-    //         <circle
-    //           cx="50%"
-    //           cy="50%"
-    //           r="60"
-    //           stroke="#FFC7D4"
-    //           strokeWidth="1"
-    //           fill="none"
-    //         />
-    //         <circle
-    //           cx="50%"
-    //           cy="50%"
-    //           r="110"
-    //           stroke="#FFC7D4"
-    //           strokeWidth="1"
-    //           fill="none"
-    //         />
-    //         <circle
-    //           cx="50%"
-    //           cy="50%"
-    //           r="160"
-    //           stroke="#FFC7D4"
-    //           strokeWidth="1"
-    //           fill="none"
-    //         />
-    //         <circle
-    //           cx="50%"
-    //           cy="50%"
-    //           r="210"
-    //           stroke="#FFC7D4"
-    //           strokeWidth="1"
-    //           fill="none"
-    //         />
-    //       </svg>
-
-    //       <div
-    //         style={{
-    //           position: "absolute",
-    //           top: "50%",
-    //           left: "50%",
-    //           transform: "translate(-50%, -50%)",
-    //           width: "80px",
-    //           height: "80px",
-    //           backgroundColor: "#fff",
-    //           borderRadius: "50%",
-    //           overflow: "hidden",
-    //           border: "1px solid #FFC7D4",
-    //         }}
-    //       >
-    //         <img
-    //           src="/images/pic1.png"
-    //           alt="Center Profile"
-    //           style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    //         />
-    //       </div>
-    //       {/* Surrounding circles */}
-    //       {profileImages.map((src, index) => (
-    //         <div
-    //           key={index}
-    //           style={{
-    //             position: "absolute",
-    //             ...profilePositions[index],
-    //             width: "50px",
-    //             height: "50px",
-    //             backgroundColor: "#fff",
-    //             borderRadius: "50%",
-    //             overflow: "hidden",
-    //             border: "1px solid #FFC7D4",
-    //           }}
-    //         >
-    //           <img
-    //             src={src}
-    //             alt={`Profile ${index + 1}`}
-    //             style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    //           />
-    //         </div>
-    //       ))}
-    //       {/* Red Circles */}
-    //       {redCirclePositions.map((style, index) => (
-    //         <div
-    //           key={index}
-    //           style={{
-    //             position: "absolute",
-    //             width: `${redCircleSizes[index]}px`,
-    //             height: `${redCircleSizes[index]}px`,
-    //             ...style,
-    //           }}
-    //         >
-    //           <img
-    //             src="/images/red.png"
-    //             alt={`Profile ${index + 1}`}
-    //             style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    //           />
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
-    //old code :
-    <div style={{ backgroundColor: "#EAF2FF", position: "relative" }}>
-      <section
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          // marginTop: 104,
-          marginLeft: 150,
-          paddingTop: 50,
-          paddingBottom: 100,
-          textAlign: "left",
-        }}
-      >
-        <div style={{ maxWidth: "400px" }}>
-          <h3
-            style={{
-              color: "#23a6f0",
-              width: 200,
-              height: 24,
-              fontWeight: 1000,
-              fontSize: 16,
-              paddingBottom: 50,
-            }}
-          >
-            Online Training
-          </h3>
-          <h1
-            style={{
-              color: "#252b42",
-              width: 350,
-              fontSize: "50px",
-              fontWeight: 10000,
-            }}
-          >
-            Master Coding with Experts
-          </h1>
-          <Paragraph sx={{ fontSize: 18, paddingTop: 5 }}>
-            Learn to code from scratch and enhance your skills with industry
-            professionals.
-          </Paragraph>
-        </div>
-
-        {/* Orbiting circles */}
+    <div className="flex w-full justify-between bg-[#EAF2FF]">
+      <div className="flex flex-col gap-5 !pt-25 !pb-50 !pl-40">
+        <h6 className="text-[#23A6F0]">Online training</h6>
+        <h1 className="w-2 text-5xl leading-19 font-bold text-[#252B42]">
+          Highly Distinguished Courses
+        </h1>
+        <p className="w-90 text-xl font-light text-[#737373]">
+          We know how large objects will act, but things on a small scale
+        </p>
+      </div>
+      <div className="w-200 !pl-30 text-white">
         <div
           style={{
             position: "relative",
@@ -243,7 +89,6 @@ const HeroSection = () => {
             />
           </svg>
 
-          {/* Center circle */}
           <div
             style={{
               position: "absolute",
@@ -264,7 +109,6 @@ const HeroSection = () => {
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
           </div>
-
           {/* Surrounding circles */}
           {profileImages.map((src, index) => (
             <div
@@ -287,7 +131,6 @@ const HeroSection = () => {
               />
             </div>
           ))}
-
           {/* Red Circles */}
           {redCirclePositions.map((style, index) => (
             <div
@@ -307,8 +150,165 @@ const HeroSection = () => {
             </div>
           ))}
         </div>
-      </section>
+      </div>
     </div>
+    //old code :
+    // <div style={{ backgroundColor: "#EAF2FF", position: "relative" }}>
+    //   <section
+    //     style={{
+    //       display: "flex",
+    //       alignItems: "center",
+    //       justifyContent: "space-between",
+    //       // marginTop: 104,
+    //       marginLeft: 150,
+    //       paddingTop: 50,
+    //       paddingBottom: 100,
+    //       textAlign: "left",
+    //     }}
+    //   >
+    //     <div style={{ maxWidth: "400px" }}>
+    //       <h3
+    //         style={{
+    //           color: "#23a6f0",
+    //           width: 200,
+    //           height: 24,
+    //           fontWeight: 1000,
+    //           fontSize: 16,
+    //           paddingBottom: 50,
+    //         }}
+    //       >
+    //         Online Training
+    //       </h3>
+    //       <h1
+    //         style={{
+    //           color: "#252b42",
+    //           width: 350,
+    //           fontSize: "50px",
+    //           fontWeight: 10000,
+    //         }}
+    //       >
+    //         Master Coding with Experts
+    //       </h1>
+    //       <Paragraph sx={{ fontSize: 18, paddingTop: 5 }}>
+    //         Learn to code from scratch and enhance your skills with industry
+    //         professionals.
+    //       </Paragraph>
+    //     </div>
+
+    //     {/* Orbiting circles */}
+    //     <div
+    //       style={{
+    //         position: "relative",
+    //         marginRight: 70,
+    //         width: "500px",
+    //         height: "500px",
+    //       }}
+    //     >
+    //       <svg
+    //         width="100%"
+    //         height="100%"
+    //         style={{ position: "absolute", top: 0, left: 0 }}
+    //       >
+    //         <circle
+    //           cx="50%"
+    //           cy="50%"
+    //           r="60"
+    //           stroke="#FFC7D4"
+    //           strokeWidth="1"
+    //           fill="none"
+    //         />
+    //         <circle
+    //           cx="50%"
+    //           cy="50%"
+    //           r="110"
+    //           stroke="#FFC7D4"
+    //           strokeWidth="1"
+    //           fill="none"
+    //         />
+    //         <circle
+    //           cx="50%"
+    //           cy="50%"
+    //           r="160"
+    //           stroke="#FFC7D4"
+    //           strokeWidth="1"
+    //           fill="none"
+    //         />
+    //         <circle
+    //           cx="50%"
+    //           cy="50%"
+    //           r="210"
+    //           stroke="#FFC7D4"
+    //           strokeWidth="1"
+    //           fill="none"
+    //         />
+    //       </svg>
+
+    //       {/* Center circle */}
+    //       <div
+    //         style={{
+    //           position: "absolute",
+    //           top: "50%",
+    //           left: "50%",
+    //           transform: "translate(-50%, -50%)",
+    //           width: "80px",
+    //           height: "80px",
+    //           backgroundColor: "#fff",
+    //           borderRadius: "50%",
+    //           overflow: "hidden",
+    //           border: "1px solid #FFC7D4",
+    //         }}
+    //       >
+    //         <img
+    //           src="/images/pic1.png"
+    //           alt="Center Profile"
+    //           style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    //         />
+    //       </div>
+
+    //       {/* Surrounding circles */}
+    //       {profileImages.map((src, index) => (
+    //         <div
+    //           key={index}
+    //           style={{
+    //             position: "absolute",
+    //             ...profilePositions[index],
+    //             width: "50px",
+    //             height: "50px",
+    //             backgroundColor: "#fff",
+    //             borderRadius: "50%",
+    //             overflow: "hidden",
+    //             border: "1px solid #FFC7D4",
+    //           }}
+    //         >
+    //           <img
+    //             src={src}
+    //             alt={`Profile ${index + 1}`}
+    //             style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    //           />
+    //         </div>
+    //       ))}
+
+    //       {/* Red Circles */}
+    //       {redCirclePositions.map((style, index) => (
+    //         <div
+    //           key={index}
+    //           style={{
+    //             position: "absolute",
+    //             width: `${redCircleSizes[index]}px`,
+    //             height: `${redCircleSizes[index]}px`,
+    //             ...style,
+    //           }}
+    //         >
+    //           <img
+    //             src="/images/red.png"
+    //             alt={`Profile ${index + 1}`}
+    //             style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    //           />
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </section>
+    // </div>
   );
 };
 
--- my-training-website/src/app/components/WhyJoinSection.tsx
+++ my-training-website/src/app/components/WhyJoinSection.tsx
@@ -1,75 +1,120 @@
-import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
-import { CreditCard, Favorite, PhotoLibrary } from '@mui/icons-material';
+import { Box, Typography, Grid, Card, CardContent } from "@mui/material";
+import { CreditCard, Favorite, PhotoLibrary } from "@mui/icons-material";
 
 const features = [
   {
-    title: 'Lifetime access',
-    desc: 'Pay once and learn for ever ',
-    icon: <img src='/creditcard.png' width={24}/>,
+    title: "Lifetime access",
+    desc: "Pay once and learn for ever ",
+    icon: <img src="/creditcard.png" width={24} />,
   },
   {
-    title: 'Touch By Experts',
-    desc: 'Our experts recorded the sessions to their experience with you.',
-    icon: <img src='/heart.png' width={24} />,
+    title: "Touch By Experts",
+    desc: "Our experts recorded the sessions to their experience with you.",
+    icon: <img src="/heart.png" width={24} />,
   },
   {
-    title: 'Code Library',
-    desc: 'Our videos are supported by many books that you can access for free.',
-    icon: <img src='/album.png' width={24} />,
+    title: "Code Library",
+    desc: "Our videos are supported by many books that you can access for free.",
+    icon: <img src="/album.png" width={24} />,
   },
 ];
 
 const WhyJoinSection = () => {
   return (
-    <Box sx={{ py: 10, px: 10, bgcolor: '#fff', maxWidth: "1000px", mx: "auto"}}>
-      <h3 style={{ color: '#23a6f0',width: 128,
-                      height: 24,
-                      fontWeight:1000,
-                      fontSize:16, }}>
-        Our Goals
-      </h3>
-      <Typography variant="h4" fontWeight={900} sx={{color: '#252b42', mt: 2 }}>
-        Why you should join?
-      </Typography>
-      <Typography sx={{ fontSize:14, color: '#666', maxWidth: 600, mt: 2 }}>
-      We are not just another coding platform—we are your gateway to success in tech. Learn, practice, and build with us.
-      </Typography>
+    <div className="flex w-full justify-between text-[14px] text-[#252B42]">
+      <div className="gap-4.5 !pt-15 !pb-50 !pl-37">
+        <h6 className="text-[#23A6F0]">Practice Advice</h6>
+        <h1 className="text-4xl leading-18 font-bold text-[#252B42]">
+          Why you should join?
+        </h1>
+        <p className="w-160 font-light text-[#737373]">
+          We are not the only online training platform but we aim to provide you
+          best learning experience
+        </p>
+        <div className="!mt-24 flex h-[280px] flex-row gap-6">
+          <div className="w-75 bg-[#E8F1FF] !p-10 shadow-lg">
+            <div className="!mb-5 flex h-[66] w-[60] items-center justify-center rounded-lg bg-[#23A6F0] !p-4.5">
+              <img src="/creditcard.png" />
+            </div>
+            <h4 className="text-[16px]">Lifetime access</h4>
+            <Box sx={{ width: 50, height: 2, bgcolor: "#E74040", my: 2 }} />
+            <p className="w-160 font-light text-[#737373]">
+              Pay once and learn for ever
+            </p>
+          </div>
+          <div className="w-75 bg-[#E8F1FF] !p-10 shadow-lg">
+            <div className="!mb-5 flex h-[66] w-[60] items-center justify-center rounded-lg bg-[#23A6F0] !p-4.5">
+              <img src="/heart.png" />
+            </div>
+            <h4 className="text-[16px]">Touch By Experts</h4>
+            <Box sx={{ width: 50, height: 2, bgcolor: "#E74040", my: 2 }} />
+            <p className="font-light text-[#737373]">
+              Our experts recorded the sessions to their experience with you.
+            </p>
+          </div>
+          <div className="w-75 bg-[#E8F1FF] !p-10 shadow-lg">
+            <div className="!mb-5 flex h-[66] w-[60] items-center justify-center rounded-lg bg-[#23A6F0] !p-4.5">
+              <img src="/album.png" />
+            </div>
+            <h4 className="text-[16px]">Books Liberary</h4>
+            <Box sx={{ width: 50, height: 2, bgcolor: "#E74040", my: 2 }} />
+            <p className="w-auto font-light text-[#737373]">
+              Our videos are supported by many books that you can access for
+              free
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    // <Box sx={{ py: 10, px: 10, bgcolor: '#fff', maxWidth: "1000px", mx: "auto"}}>
+    //   <h3 style={{ color: '#23a6f0',width: 128,
+    //                   height: 24,
+    //                   fontWeight:1000,
+    //                   fontSize:16, }}>
+    //     Our Goals
+    //   </h3>
+    //   <Typography variant="h4" fontWeight={900} sx={{color: '#252b42', mt: 2 }}>
+    //     Why you should join?
+    //   </Typography>
+    //   <Typography sx={{ fontSize:14, color: '#666', maxWidth: 600, mt: 2 }}>
+    //   We are not just another coding platform—we are your gateway to success in tech. Learn, practice, and build with us.
+    //   </Typography>
 
-      <Grid container spacing={4} sx={{ mt: 7 }}>
-        {features.map((feature, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
-            <Card sx={{ bgcolor: '#E8F1FF', borderRadius: 0, p: 4, height:'250px', paddingBottom:28 }}>
-              <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+    //   <Grid container spacing={4} sx={{ mt: 7 }}>
+    //     {features.map((feature, index) => (
+    //       <Grid item key={index} xs={12} sm={6} md={4}>
+    //         <Card sx={{ bgcolor: '#E8F1FF', borderRadius: 0, p: 4, height:'250px', paddingBottom:28 }}>
+    //           <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
 
-                <Box
-                  sx={{
-                    width: 60, 
-                    height: 66, 
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#23A6F0", 
-                    borderRadius: 2, 
-                  }}
-                >
-                  {feature.icon}
-                </Box>
-              </Box>
+    //             <Box
+    //               sx={{
+    //                 width: 60,
+    //                 height: 66,
+    //                 display: "flex",
+    //                 alignItems: "center",
+    //                 justifyContent: "center",
+    //                 backgroundColor: "#23A6F0",
+    //                 borderRadius: 2,
+    //               }}
+    //             >
+    //               {feature.icon}
+    //             </Box>
+    //           </Box>
 
-              <CardContent sx={{ p: 0, textAlign: 'left' }}>
-                <Typography variant="h3" fontWeight={900} fontSize={16} >
-                  {feature.title}
-                </Typography>
+    //           <CardContent sx={{ p: 0, textAlign: 'left' }}>
+    //             <Typography variant="h3" fontWeight={900} fontSize={16} >
+    //               {feature.title}
+    //             </Typography>
 
-                <Box sx={{ width: 50, height: 2, bgcolor: '#E74040', my: 2 }} />
+    //             <Box sx={{ width: 50, height: 2, bgcolor: '#E74040', my: 2 }} />
 
-                <Typography sx={{ fontSize:14, mt: 1, color: '#666' }}>{feature.desc}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    //             <Typography sx={{ fontSize:14, mt: 1, color: '#666' }}>{feature.desc}</Typography>
+    //           </CardContent>
+    //         </Card>
+    //       </Grid>
+    //     ))}
+    //   </Grid>
+    // </Box>
   );
 };
 
